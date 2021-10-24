e2fdca0721863b1bfe87dd27592d80366b4698d4
Chris Ferdian <chris.ferdian@netzme.id>
2021-10-24 21:54:07 +0700

    Completed MainCollectionViewCell Layout and data binding function

--- TokyoSplash/Modules/Main/Cell/MainCollectionViewCell.swift
+++ TokyoSplash/Modules/Main/Cell/MainCollectionViewCell.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MainCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelDescription: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func bind(with photo: Photo) {
+        self.labelName.text = photo.user?.name
+        self.labelDescription.text = photo.photoDescription
+        if let urlString = photo.urls?.regular, let url = URL(string: urlString) {
+            self.imageView.kf.setImage(with: url, placeholder: nil, options: nil, completionHandler: nil)
+        }
     }
 
 }
