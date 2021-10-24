f19e1a682746865795079d55ad97b8bbdb880e11
Chris Ferdian <chris.ferdian@netzme.id>
2021-10-24 07:58:30 +0700

    added collection view layout list and grid

---
+++ TokyoSplash/Helpers/TokyoCollectionLayout.swift
@@ -0,0 +1,55 @@
+//
+//  TokyoCollectionLayout.swift
+//  TokyoSplash
+//
+//  Created by Chris Ferdian on 24/10/21.
+//
+
+import UIKit
+
+enum CollectionStyle {
+    case list
+    case grid
+}
+
+class TokyoCollectionLayout : UICollectionViewFlowLayout {
+    
+    var display : CollectionStyle = .list {
+        didSet {
+            if display != oldValue {
+                self.invalidateLayout()
+            }
+        }
+    }
+    
+    convenience init(display: CollectionStyle) {
+        self.init()
+        
+        self.display = display
+        self.minimumLineSpacing = 1
+        self.minimumInteritemSpacing = 1
+        self.configLayout()
+    }
+    
+    func configLayout() {
+        switch display {
+        case .grid:
+            self.scrollDirection = .vertical
+            if let collectionView = self.collectionView {
+                let optimisedWidth = (collectionView.frame.width - minimumInteritemSpacing) / 2
+                let height = collectionView.frame.height / 2.5
+                self.itemSize = CGSize(width: optimisedWidth , height: height) // keep as square
+            }
+        case .list:
+            self.scrollDirection = .vertical
+            if let collectionView = self.collectionView {
+                self.itemSize = CGSize(width: collectionView.frame.width , height: 130)
+            }
+        }
+    }
+    
+    override func invalidateLayout() {
+        super.invalidateLayout()
+        self.configLayout()
+    }
+}
--- TokyoSplash/Modules/Main/MainViewController.swift
+++ TokyoSplash/Modules/Main/MainViewController.swift
@@ -9,9 +9,44 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segmentedViewType: UISegmentedControl!
+    
+    lazy var collectionViewFlowLayout : TokyoCollectionLayout = {
+        let layout = TokyoCollectionLayout(display: .grid)
+        return layout
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "写真"
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = collectionViewFlowLayout
+        collectionViewFlowLayout.display = .list
+    }
     
+    @IBAction func didTapSegmented(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            collectionViewFlowLayout.display = .list
+            collectionViewFlowLayout.invalidateLayout()
+        } else {
+            collectionViewFlowLayout.display = .grid
+            collectionViewFlowLayout.invalidateLayout()
+        }
+    }
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath)
+        cell.backgroundColor = .systemYellow
+        return cell
     }
 }
